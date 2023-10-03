e2d46f8bba7840a3e78627b2b09a5cadad74aa74
iso12800jp <iso12800jp@yahoo.co.jp>
2023-10-03 13:06:31 +0900

    型とかいろいろ間違ってたので訂正

--- make_ime_dic.py
+++ make_ime_dic.py
@@ -2,12 +2,12 @@
 import json
 
 
-with open("./emojis.json", "r") as emoji_file_en:
+with open("./emojis_ja.json", "r") as emoji_file_en:
     emojis_name_list_en = list(json.load(emoji_file_en).items())
 
 output_list = []
-for emoji_pair in emoji_file_en:
+for emoji_pair in emojis_name_list_en:
     output_list.append(f":{emoji_pair[0]}\t{emoji_pair[1]}\t記号\n")
 
-with open("./emojis_ja.json", "w") as emoji_file_ja:
-    emoji_file_ja.write("output_list.join()")
+with open("emoji.txt", "w") as emoji_file_ja:
+    emoji_file_ja.write("".join(output_list))
